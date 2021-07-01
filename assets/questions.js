0f9bdced28079b6b6056c8d320da78b01bde779d
sylv4030 <77740560+sylv4030@users.noreply.github.com>
2021-06-30 21:25:49 -0500

    First commit: script, html, css

---
+++ assets/questions.js
@@ -0,0 +1,70 @@
+//moved questions to seperate JS page
+
+var questionsArray = [
+    {
+        question: "What does CSS stand for?",
+        answer1: "Cascading Style Sheets",
+        answer2: "Cat Cow Corn",
+        answer3: "Cussing",
+        answer4: "Clack Stack Shack",
+        correctAnswer: "1"}, 
+    
+    
+    
+    {   
+        question: "What does HTML stand for?",
+        answer1: "Hot Time Morning Light",
+        answer2: "Hit Me Last",
+        answer3: "hlmte",
+        answer4: "Hypertext Markup Language",
+        correctAnswer: "4"
+    },
+    {   
+        question: "What does JS stand for?",
+        answer1: "Jeepers Scoopers",
+        answer2: "Java Script",
+        answer3: "Java Shmush",
+        answer4: "Jack Smack",
+        correctAnswer: "2"
+    },
+    {   
+        question: "What does HTML stand for?",
+        answer1: "Hot Time Morning Light",
+        answer2: "Hit Me Last",
+        answer3: "hlmte",
+        answer4: "Hypertext Markup Language",
+        correctAnswer: "4"
+    },
+    {   
+        question: "What does HTML stand for?",
+        answer1: "Hot Time Morning Light",
+        answer2: "Hit Me Last",
+        answer3: "hlmte",
+        answer4: "Hypertext Markup Language",
+        correctAnswer: "4"
+    },
+    {   
+        question: "What does HTML stand for?",
+        answer1: "Hot Time Morning Light",
+        answer2: "Hit Me Last",
+        answer3: "hlmte",
+        answer4: "Hypertext Markup Language",
+        correctAnswer: "4"
+    },
+    {   
+        question: "What does HTML stand for?",
+        answer1: "Hot Time Morning Light",
+        answer2: "Hit Me Last",
+        answer3: "hlmte",
+        answer4: "Hypertext Markup Language",
+        correctAnswer: "4"
+    },
+    {   
+        question: "What does HTML stand for?",
+        answer1: "Hot Time Morning Light",
+        answer2: "Hit Me Last",
+        answer3: "hlmte",
+        answer4: "Hypertext Markup Language",
+        correctAnswer: "4"
+    },
+    ]
---
+++ assets/script.js
@@ -0,0 +1,73 @@
+//global variables
+    //setting vaibles for HTML elements
+var questionAsk = document.getElementById("questions");
+var timer = document.getElementById("time");
+var scoreBoard = document.getElementById("score");
+var start = document.getElementById("strtButton");
+
+var button1 = document.getElementById("1");
+var button2 = document.getElementById("2");
+var button3 = document.getElementById("3");
+var button4 = document.getElementById("4");
+
+// variables for functions
+var questionOptions = 0;
+var timerSetting = 60;
+var timerInterval;
+var correct;
+var score = 1;
+
+//function to start the game and timer
+
+function startApp(){
+    
+    timerInterval = setInterval(function() {
+        timerSetting--;
+        timer.textContent = "Time: " + timerSetting;
+
+        if(timerSetting === 0) {
+            clearInterval(timerInterval);
+            alert("Game Over!");
+            alert("Your score is: " + score);
+            prompt("Enter your initials: " );
+        }
+    }, 1000);
+    questionSelect()
+
+    }
+
+start.addEventListener("click", startApp);
+
+//function to select question from array and text for question HTML
+
+function questionSelect() {
+    var currentQuestion = questionsArray[questionOptions];
+    questionAsk.innerHTML = "<h2>" + currentQuestion.question;
+    button1.innerHTML = currentQuestion.answer1;
+    button2.innerHTML = currentQuestion.answer2;
+    button3.innerHTML = currentQuestion.answer3;
+    button4.innerHTML = currentQuestion.answer4;
+};
+
+//checking if asnwer is correct and adjusting score and time and then moving to next question
+
+function confirmAnswer(answer){
+    correct = questionsArray[questionOptions].correctAnswer
+        if(answer === correct){
+            questionOptions++;
+            alert("You are right!");
+            scoreBoard.textContent = "Score: "  + score++;
+            questionSelect()
+           
+        }
+        else if(answer !== correct){
+            questionOptions++;
+            alert("Wrong answer!");
+            timerSetting -= 5;
+            questionSelect()
+            
+        }
+}
+
+
+
